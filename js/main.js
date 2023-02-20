300891cc2132ac2f9b53e22e879c5caf00778663
giovannifranchi <giovanni.franchi95@gmail.com>
2023-02-20 16:32:44 +0100

    Feat: js prompts

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const passwordText = document.querySelector('#password-text');
+const button = document.querySelector('.btn');
+
+button.addEventListener('click', ()=> {
+    const name = prompt('enter your name', '');
+    const lastName = prompt ('enter your last name', '');
+    const color = prompt('enter your favourite color', '');
+    const password = `${name}${lastName}${color}` 
+    passwordText.innerText = `Your Password is: \r\n ${password}`
+})
